83de96cfa37ca8ebdc73d5fa02be1c04e691d30c
祝传鹏 <Luomusha@gmail.com>
2020-10-11 09:27:24 +0800

    build

--- dist/router/AuthUserRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const AuthUserController_1 = require("../controller/AuthUserController");
-const authUserRouter = new Router();
-authUserRouter.post('/', AuthUserController_1.beforePostUser, AuthUserController_1.postUser);
-authUserRouter.put('/', AuthUserController_1.beforePutUser, AuthUserController_1.putUser);
-exports.default = authUserRouter;
--- dist/router/TokenRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const TokenController_1 = require("../controller/TokenController");
-const tokenRouter = new Router();
-tokenRouter.get('/', TokenController_1.generateToken);
-exports.default = tokenRouter;
